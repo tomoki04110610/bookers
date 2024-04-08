1ba3ae09869601e1222e12e47be336435dd2b3cd
EC2 Default User <ec2-user@ip-172-31-6-186.ap-northeast-1.compute.internal>
2024-04-08 08:34:28 +0000

    バリデーション、create.update,遷移先showに変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to books_path
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
       render :new
     end
@@ -27,7 +28,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
