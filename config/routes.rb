dec1d20bba1aec37d5fdba8741330e8e5f67503c
EC2 Default User <ec2-user@ip-172-31-6-186.ap-northeast-1.compute.internal>
2024-04-08 01:40:24 +0000

    resources :booksに変更各url、パス変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show',as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
